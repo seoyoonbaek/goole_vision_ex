460af7e7ec5bd3a8c9acc8dd829ac0bc8d205b8e
baekseoyoon <bsj8993@naver.com>
2020-05-26 09:25:14 +0900

    sy test

---
+++ app/src/main/java/com/example/goole_vision_ex/Condition.java
@@ -0,0 +1,5 @@
+package com.example.goole_vision_ex;
+
+public enum Condition {
+    USER_EYES_OPEN, USER_EYES_CLOSED, FACE_NOT_FOUND
+}
---
+++ app/src/main/java/com/example/goole_vision_ex/CountDown.java
@@ -0,0 +1,10 @@
+package com.example.goole_vision_ex;
+
+import android.widget.TextView;
+
+public class CountDown {
+    TextView countdown_text = (TextView)findViewById(R.id.countdown_text);
+    String conversionTime = "000010";
+    countDown(conversionTime);
+
+}
